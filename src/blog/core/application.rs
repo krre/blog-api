d83ff630b3115f4b51cd03b634c8d11c64ee3377
Vladimir Zarypov <krre31@gmail.com>
2026-03-09 07:23:03 +0200

    Add config

--- src/blog/core/application.rs
+++ src/blog/core/application.rs
@@ -1,15 +1,34 @@
 use axum::{Router, routing::get};
+use serde::Deserialize;
 
-pub struct Application {}
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    host: String,
+    port: u16,
+}
+
+pub struct Application {
+    config: Config,
+}
+
+impl Config {
+    fn from_env() -> Self {
+        envy::from_env().expect("Failed to load config")
+    }
+}
 
 impl Application {
     pub fn new() -> Self {
-        Self {}
+        let config = Config::from_env();
+
+        Self { config }
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         let app = Router::new().route("/", get(handler));
-        let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+        let listener =
+            tokio::net::TcpListener::bind(format!("{}:{}", self.config.host, self.config.port))
+                .await?;
         println!("listening on {}", listener.local_addr()?);
         axum::serve(listener, app).await?;
         Ok(())
