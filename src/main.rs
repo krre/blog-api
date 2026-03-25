ca0f1343f93f28d026a760650ae260dc00910087
Vladimir Zarypov <krre31@gmail.com>
2026-03-25 07:58:47 +0200

    Use `dotenvy` crate to load environment variables from .env file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use blog::core::Application;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv().ok();
     Application::new().run().await?;
     Ok(())
 }
