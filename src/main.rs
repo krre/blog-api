724b30ee9387bd0864ee9867bcdeefa5fe61d25b
Vladimir Zarypov <krre31@gmail.com>
2026-04-07 08:30:42 +0300

    Enable JSON log formatting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use blog::core::Application;
 use tracing_subscriber::{EnvFilter, prelude::*};
 
+fn init_tracing() {
+    let filter_layer = EnvFilter::from_default_env();
+    let fmt_layer = tracing_subscriber::fmt::layer().without_time().json();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv().ok();
-
-    tracing_subscriber::registry()
-        .with(EnvFilter::from_default_env())
-        .with(
-            tracing_subscriber::fmt::layer()
-                .without_time()
-                .with_ansi(false),
-        )
-        .init();
+    init_tracing();
 
     Application::new().run().await?;
     Ok(())
