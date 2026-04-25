c609946e61a0c46e4f5ca671e27567a7799c14b0
Vladimir Zarypov <krre31@gmail.com>
2026-04-25 12:27:03 +0300

    Add Postgres support

--- src/blog/core/application.rs
+++ src/blog/core/application.rs
@@ -1,10 +1,12 @@
 use axum::Router;
+use sea_orm::{Database, DatabaseConnection};
 use serde::Deserialize;
 use tracing::info;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
     server_addr: String,
+    postgres_url: String,
 }
 
 pub struct Application {
@@ -20,11 +22,14 @@ impl Config {
 impl Application {
     pub fn new() -> Self {
         let config = Config::from_env();
+        println!("{:?}", config);
 
         Self { config }
     }
 
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let db: DatabaseConnection = Database::connect(&self.config.postgres_url).await?;
+
         let listener = tokio::net::TcpListener::bind(&self.config.server_addr).await?;
         let router = Router::new();
 
