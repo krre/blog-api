b6e22ed0c99542a0996c069210a87750be22eb12
Vladimir Zarypov <krre31@gmail.com>
2026-05-15 08:41:53 +0300

    Add logging HTTP method

--- src/blog/api/middleware/console.rs
+++ src/blog/api/middleware/console.rs
@@ -18,21 +18,30 @@ pub async fn log_request_response(
     next: Next,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     let (parts, body) = req.into_parts();
+    let method = parts.method.to_string();
+
     let client_ip = parts
         .headers
         .get("x-forwarded-for")
         .and_then(|v| v.to_str().ok())
         .map(|s| s.to_string())
         .unwrap_or_else(|| "Unknown".to_string());
+
     let path = parts.uri.path().to_string();
-    let bytes = buffer_and_print(Direction::Request, &client_ip, &path, body).await?;
+    let bytes = buffer_and_print(Direction::Request, &method, &client_ip, &path, body).await?;
     let req = Request::from_parts(parts, Body::from(bytes));
 
     let res = next.run(req).await;
 
     let (parts, body) = res.into_parts();
-    let bytes =
-        buffer_and_print(Direction::Response(parts.status), &client_ip, &path, body).await?;
+    let bytes = buffer_and_print(
+        Direction::Response(parts.status),
+        &method,
+        &client_ip,
+        &path,
+        body,
+    )
+    .await?;
     let res = Response::from_parts(parts, Body::from(bytes));
 
     Ok(res)
@@ -40,6 +49,7 @@ pub async fn log_request_response(
 
 async fn buffer_and_print<B>(
     direction: Direction,
+    method: &str,
     client_ip: &str,
     path: &str,
     body: B,
@@ -65,7 +75,7 @@ where
 
     if let Ok(body) = std::str::from_utf8(&bytes) {
         match direction {
-            Direction::Request => log_request(message_type, client_ip, path, body),
+            Direction::Request => log_request(message_type, method, client_ip, path, body),
             Direction::Response(status) => {
                 log_response(message_type, client_ip, path, status, body)
             }
@@ -75,11 +85,19 @@ where
     Ok(bytes)
 }
 
-fn log_request(message_type: &str, client_ip: &str, path: &str, message: &str) {
+fn log_request(message_type: &str, method: &str, client_ip: &str, path: &str, message: &str) {
     if path.starts_with("/auth/login") {
-        log_mask_value(message_type, client_ip, path, None, message, "password");
+        log_mask_value(
+            message_type,
+            Some(method),
+            client_ip,
+            path,
+            None,
+            message,
+            "password",
+        );
     } else {
-        tracing::info!(message_type, client_ip, path, message);
+        tracing::info!(message_type, method, client_ip, path, message);
     }
 }
 
@@ -110,6 +128,7 @@ fn log_response(
         if path.starts_with("/auth/login") {
             log_mask_value(
                 message_type,
+                None,
                 client_ip,
                 path,
                 Some(status),
@@ -130,6 +149,7 @@ fn log_response(
 
 fn log_mask_value(
     message_type: &str,
+    method: Option<&str>,
     client_ip: &str,
     path: &str,
     status: Option<StatusCode>,
@@ -144,8 +164,8 @@ fn log_mask_value(
 
                     if let Some(status) = status {
                         tracing::info!(message_type, client_ip, path, status = status.as_u16(), message = %value);
-                    } else {
-                        tracing::info!(message_type, client_ip, path, message = %value);
+                    } else if let Some(method) = method {
+                        tracing::info!(message_type, method, client_ip, path, message = %value);
                     }
                 }
             }
