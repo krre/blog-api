57cdd862f609c4d0c22090bb9a83d8bd3b4e5ed0
Vladimir Zarypov <krre31@gmail.com>
2026-05-21 07:36:22 +0300

    Add mirgration to create `posts` table

---
+++ migrations/20260521041940_create_posts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id bigserial PRIMARY KEY,
+    user_id int8 NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    post text NOT NULL DEFAULT '',
+    is_published bool NOT NULL DEFAULT false,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    published_at timestamptz
+);
+
+CREATE INDEX IF NOT EXISTS posts_user_id_idx ON posts (user_id);
