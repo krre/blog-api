10905fbefff281e5634bf7f4960fbb6acdb14267
Vladimir Zarypov <krre31@gmail.com>
2026-06-13 13:08:36 +0300

    Merge all migrations into one

--- migrations/20260613100300_create_all.sql
+++ migrations/20260613100300_create_all.sql
@@ -12,3 +12,15 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (username, first_name) VALUES('admin', 'Admin');
+
+CREATE TABLE IF NOT EXISTS posts (
+    id bigserial PRIMARY KEY,
+    user_id int8 NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    title text NOT NULL,
+    post text NOT NULL DEFAULT '',
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz NOT NULL DEFAULT now(),
+    published_at timestamptz
+);
+
+CREATE INDEX IF NOT EXISTS posts_user_id_idx ON posts (user_id);
